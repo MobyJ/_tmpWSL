a9819cd54dd86ba5e954264165209207af5fc21b
sebastien COLLE <scolle@sii.fr>
2020-10-01 12:14:00 +0200

    TST Sh : get host info

--- tst.py
+++ tst.py
@@ -4,3 +4,18 @@ print("HelloWorld")
 
 for i in range(32, 126):
     print( i, chr(i) )
+
+
+words = ['apple', 'banana', 'peach', '42']
+if 'apple' in words:
+    print('found apple')
+
+if 'a' in words:
+    print('found a')
+else:
+    print('NOT found a')
+
+if 42 in words:
+    print('found 42')
+else:
+    print('NOT found 42')
--- tst.sh
+++ tst.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+echo $HOSTNAME
+uname -o
+uname -r
+uname -a
+
+cat /etc/os-release | grep PRETTY_NAME
+cat /etc/debian_version 
 echo "this is a testing display !!!"
